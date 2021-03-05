41e2d63367a424ef5bca759492d2f48e86a85c4b
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-03-05 11:41:04 +0000

    Carousel functioning

--- Scripts/slideshow.js
+++ Scripts/slideshow.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $('.your-class').slick({
-      setting-name: setting-value
+    $('.slide').slick({
+      slidesToShow: 1,
+      // slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      prevArrow: false,
+      nextArrow: false
     });
   });
+
