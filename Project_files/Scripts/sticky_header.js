cf5529d65f95e2b19506c1b77a61b8858dcebda2
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-03-11 01:35:05 +0000

    working on stickyheader.
    Changed class to id, has styling issues, may need to revert

--- Project_files/Scripts/sticky_header.js
+++ Project_files/Scripts/sticky_header.js
@@ -1,8 +1,25 @@
 // header needs to be fixed when scrolling up
 
-Window.addEventListener('scroll', function() {
-    
+const stickyHead = document.getElementById('header_container');
+
+let scrolling = false;
+
+window.scroll = () => {
+    scrolling = true;
+};
+
+window.addEventListener('scroll', function() {
+
+ 
+    if (window.pageYOffset > 200){
+        stickyHead.style.position = "fixed";
+        console.log('scroll working');
+    } else { 
+        stickyHead.style.position = "static";
+    }
 });
+
+setI
 
 
 
