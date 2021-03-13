f8ccab6bad322190c368af88d7a44610d21ed0dd
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-03-13 13:46:44 +0000

    changes

--- Project_files/Scripts/sticky_header.js
+++ Project_files/Scripts/sticky_header.js
@@ -1,35 +1,18 @@
 // header needs to be fixed when scrolling up
 
-const stickyHead = document.getElementById('header_container');
-
-let scrolling = false;
-
-window.scroll = () => {
-    scrolling = true;
-};
+const stickyHead = document.getElementById('sticky_head');
 
 let scrollPosition = 0;
 
 window.addEventListener('scroll', function() {
     console.log(document.documentElement.scrollTop + ' : ' + scrollPosition);
+
+    
  
-    if (window.pageYOffset > 200){
-        stickyHead.style.position = "fixed";
-        // console.log('scroll working');
-    } else { 
-        stickyHead.style.position = "static";
-    }
+    if 
 
     scrollPosition = document.documentElement.scrollTop;
 });
-
-// setI
-
-
-
-//.header_container
-//.nav_menu
-
 
 // when scrolling down header is no longer fixed
 // this does not work when sticky header is seen on top of screen
