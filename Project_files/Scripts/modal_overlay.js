c6c37a057e821dd604544e1017c673c6da122e0c
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-03-13 16:04:49 +0000

    commit

--- Project_files/Scripts/modal_overlay.js
+++ Project_files/Scripts/modal_overlay.js
@@ -1,9 +1,10 @@
 
 // Set Variables
+const bodyElement = document.getElementById("body")
 const acceptButton = document.getElementById("Accept_Cookies")
 const modalWindow = document.getElementById("modal_container")
 const modalOverlay = document.getElementById("modal_overlay")
-let enableScrolling = false
+// let enableScrolling = false
 
 // let  Only available in the scope it is declared in
 // const  Once assigned the value cannot change
@@ -12,65 +13,54 @@ let enableScrolling = false
 
 document.addEventListener("DOMContentLoaded", function() {
 
+    console.log("loaded")
     // This is the override to test the modal
     // localStorage.setItem("accepted", "apple")
 
-    if (localStorage.getItem("accepted") == "true") {
-        hideModal()
-    } else if (localStorage.getItem("accepted") == null || 
-    localStorage.getItem("accepted") != "true") {
-        disableScrolling()
+    if (localStorage.getItem("accepted") == null || 
+        localStorage.getItem("accepted") != "true") {
+        // disableScrolling()
+        console.log("cookies not accepted")
+        showModal()
     }
-
-    let value = 6
-    // testFunction(value)
-
-    console.log(testFunction(value))
-
 })
 
 
 // Close the modal and overlay
 acceptButton.addEventListener("click", function() {
     hideModal()
-    enableScrolling = true
+    console.log("accept button pressed")
+    // enableScrolling()
     localStorage.setItem("accepted", "true")
 })
 
 // Define Functions
-function noScroll() {
-    if (!enableScrolling) {
-        window.scrollTo(0,0);
-    } else {
-        document.removeEventListener("scroll", noScroll)
-    }
+// function noScroll() {
+//     if (!enableScrolling) {
+//         window.scrollTo(0,0);
+//     } else {
+//         document.removeEventListener("scroll", noScroll)
+//     }
+// }
+
+// Only use this when accept button is pressed
+function hideModal() {
+    console.log("hide")
+    modalWindow.classList.remove("activate")
+    modalOverlay.classList.remove("activate")
 }
 
-function hideModal() {
-    modalWindow.classList.add("deactivate")
-    modalOverlay.classList.add("deactivate")
+// Only use this if they haven't accepted cookies
+function showModal() {
+    console.log("show")
+    modalWindow.classList.add("activate")
+    modalOverlay.classList.add("activate")
 }
 
 function disableScrolling() {
-    document.addEventListener("scroll", noScroll)
+    bodyElement.classList.add("disableScrolling")
 }
 
-function testFunction(_testValue) {
-
-    // console.log(_testValue)
-    let foundValue
-    for (var x = 0; x < 10; x++) {
-        if (x == _testValue) {
-            foundValue = x
-        }
-    }
-
-    return "The found value is " + foundValue
+function enableScrolling() {
+    bodyElement.classList.remove("disableScrolling")
 }
-
-
-
-
-
-
-
--- Project_files/Scripts/sticky_header.js
+++ Project_files/Scripts/sticky_header.js
@@ -2,16 +2,47 @@
 
 const stickyHead = document.getElementById('sticky_head');
 
-let scrollPosition = 0;
+let buffer = 0
+let oldScrollPosition = 0;
+let newnewScrollPosition
 
+// When we detect scrolling
 window.addEventListener('scroll', function() {
-    console.log(document.documentElement.scrollTop + ' : ' + scrollPosition);
+    // Get the value
+    newScrollPosition = document.documentElement.scrollTop
 
+    let ua = navigator.userAgent;
+    /* MSIE used to detect old browsers and Trident used to newer ones*/
+    let is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+    // not IE 
+    if (!is_ie) {
+        if (newScrollPosition > oldScrollPosition) {
+            console.log('going down');
+        } else {
+            console.log('going up');
+        }
+
+        oldScrollPosition = newScrollPosition;
+    }
+
+    // IE 'bounces up' when downwards scrolling to help content loading. When comparing 'old' and 'new' this 'bounce up' always returned zero
     
- 
-    if 
+    // is IE
 
-    scrollPosition = document.documentElement.scrollTop;
+    // != means not equal to
+
+    // (newScrollPosition - oldScrollPosition) != 0 checks for the 'bounce up'
+    if (is_ie && (newScrollPosition - oldScrollPosition) != 0) {
+        if (newScrollPosition > oldScrollPosition) {
+            console.log('going down');
+        } else {
+            console.log('going up');
+        }
+
+        oldScrollPosition = newScrollPosition;
+
+    }
 });
 
 // when scrolling down header is no longer fixed
