6104a64b4cd5146751be39501b101efc05736ad7
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-04-10 20:10:04 +0100

    Update burger_menu.js

--- Project_files/Scripts/burger_menu.js
+++ Project_files/Scripts/burger_menu.js
@@ -3,3 +3,7 @@
     //lines then transform to a cross
 
 //side menu pops up
+    // add margin to body
+    // overflow:hidden
+    // use element from other js to lock screen / shadow the element...
+    // need a scroll function for bar...
