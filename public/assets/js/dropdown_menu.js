f8cec0dd9878ce39ab5337a2386a0f842a279691
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-06-04 11:44:10 +0100

    added js. updated

---
+++ public/assets/js/dropdown_menu.js
@@ -0,0 +1,17 @@
+// the link
+
+let dropdownButton = document.querySelector('#dropdown_btn'); 
+let hide = document.querySelector('.dropdown_content');
+let drop = false;
+
+dropdownButton.addEventListener('click', function() {
+    if (drop) {
+        hide.classList.add('hidden')
+        drop = false;
+    } else {
+        hide.classList.remove('hidden');
+        drop = true;
+    }
+});
+
+
