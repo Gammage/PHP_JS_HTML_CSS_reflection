f7b012cfbd06c9e9a82b72d0e1226d9fa40a67d4
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-06-07 11:03:05 +0100

    removed console logs

--- public/assets/js/dropdown_menu.js
+++ public/assets/js/dropdown_menu.js
@@ -4,10 +4,9 @@ let dropdownButton = document.getElementById('dropdown_btn');
 let hide = document.getElementById('hidden_content');
 let drop = false;
 
-console.log("hidden");
+// console.log("hidden");
 dropdownButton.addEventListener('click', function() {
     if (drop) {
-        console.log("dropped");
         hide.classList.remove('hidden');
         drop = false;
     } else {
